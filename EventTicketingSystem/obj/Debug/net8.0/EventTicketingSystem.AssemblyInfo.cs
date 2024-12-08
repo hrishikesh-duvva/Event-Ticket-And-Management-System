60bdee5c4d96689c5bd03aa1d8c9af2bdf83c230
hrishikesh-duvva <hrishikeshduvva@gmail.com>
2024-12-08 22:09:04 +0530

    second commit

--- EventTicketingSystem/obj/Debug/net8.0/EventTicketingSystem.AssemblyInfo.cs
+++ EventTicketingSystem/obj/Debug/net8.0/EventTicketingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventTicketingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20afeb13e0210645f9e93a403f2178cd6aa752e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventTicketingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventTicketingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
